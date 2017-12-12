6752ed8768a3f697c7632a4e7d565b8331bea60f
millet <sailor_mi@163.com>
2017-12-12 14:34:05 +0800

    base.conf.js

--- src/main/webpack.main.conf.js
+++ src/main/webpack.main.conf.js
@@ -1,9 +1,9 @@
 const baseConfig = require('../../build/base.config.js');
 
 module.exports = baseConfig({
-	htmlTitle : 'hello world',
-	htmlFileURL : 'html/index.html',
-	appDir: 'js/index/',
-	uglify: false,
-	hash:''
+	htmlTitle : 'hello world',//html的标题
+	htmlFileURL : 'html/index.html',//html生成的地址
+	appDir: 'js/index/', //静态文件的地址
+	uglify: false, //是否压缩文件
+	hash:''//固定的hash值
 })
